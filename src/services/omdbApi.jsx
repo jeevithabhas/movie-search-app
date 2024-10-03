951df278442083266006068759906bb3ad18d219
jeevithabhas <jeevithabhas20@gmail.com>
2024-10-03 12:52:33 +0530

    changed

--- src/services/omdbApi.jsx
+++ src/services/omdbApi.jsx
@@ -1,20 +1,55 @@
 const API_KEY = "d24429a3";
 const BASE_URL = `https://www.omdbapi.com/?apikey=${API_KEY}`;
 
+
+const getCachedData = (key) => JSON.parse(sessionStorage.getItem(key));
+const setCachedData = (key, data) =>
+  sessionStorage.setItem(key, JSON.stringify(data));
+
 export const fetchMovies = async (query, type = "", page = 1) => {
-  const response = await fetch(
-    `${BASE_URL}&s=${query}&type=${type}&page=${page}`
-  );
-  if (!response.ok) {
-    throw new Error("Failed to fetch movies");
+  const cacheKey = `movies-${query}-${type}-${page}`;
+  const cachedMovies = getCachedData(cacheKey);
+
+  if (cachedMovies) return cachedMovies;
+
+  try {
+    const response = await fetch(
+      `${BASE_URL}&s=${query}&type=${type}&page=${page}`
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} - Failed to fetch movies`);
+    }
+
+    const data = await response.json();
+    setCachedData(cacheKey, data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching movies:", error);
+    throw error;
   }
-  return response.json();
 };
 
 export const fetchMovieDetail = async (id) => {
-  const response = await fetch(`${BASE_URL}&i=${id}`);
-  if (!response.ok) {
-    throw new Error("Failed to fetch movie details");
+  const cacheKey = `movie-${id}`;
+  const cachedMovie = getCachedData(cacheKey);
+
+  if (cachedMovie) return cachedMovie;
+
+  try {
+    const response = await fetch(`${BASE_URL}&i=${id}`);
+
+    if (!response.ok) {
+      throw new Error(
+        `Error: ${response.status} - Failed to fetch movie details`
+      );
+    }
+
+    const data = await response.json();
+    setCachedData(cacheKey, data); 
+    return data;
+  } catch (error) {
+    console.error("Error fetching movie details:", error);
+    throw error;
   }
-  return response.json();
 };
